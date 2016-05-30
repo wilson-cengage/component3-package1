86f5c54c35e68893362907789399306b834a14b7
Cheung, Wilson <wilson.cheung@cengage.com>
2016-05-30 10:28:40 -0700

    first

---
+++ client/components/Header/chunkedComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './style.css';
+
+export default class Chunked extends React.Component {
+
+    render() {
+        return (<span>CHUNKED!</span>);
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = nodeEnv === 'production';
+
+module.exports = {
+  devtool: isProd ? 'hidden-source-map' : 'cheap-eval-source-map',
+  context: path.join(__dirname, './client'),
+  entry: {
+    'index': './index.js',
+    'component2a/component2a': './exports.js'
+  },
+  output: {
+    path: path.join(__dirname, './static/'),
+    publicPath: '/static/',   /* for bundle chunk lookup during runtime, should eventually be CMP /components/component2a/ */
+    filename: '[name].js',
+    chunkFilename: 'component2a/component2a-[id].js',
+    libraryTarget: 'umd'
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.html$/,
+        loader: 'file',
+        query: {
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.css$/,
+        loaders: [
+          'style',
+          'css'
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: [
+          // 'react-hot',
+          'babel-loader'
+        ]
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    modules: [
+      path.resolve('./client'),
+      'node_modules'
+    ]
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      },
+      sourceMap: false
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+    })
+  ],
+  devServer: {
+    contentBase: './client'
+    // hot: true
+  }
+};
