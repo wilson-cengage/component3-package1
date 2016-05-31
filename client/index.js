689aa7c03ff0a47d845ad0df708ceea1ba6ef4af
Cheung, Wilson <wilson.cheung@cengage.com>
2016-05-30 17:38:39 -0700

    With gulp postbuild

--- client/index.js
+++ client/index.js
@@ -3,15 +3,21 @@ import React from 'react';
 
 import DropDown from 'components/DropDown';
 import Carousel from 'components/Carousel';
+import Email from 'components/Email';
 
 import 'index.html';
 
 ReactDOM.render(
-  <DropDown/>,
-  document.getElementById('root')
+    <DropDown/>,
+    document.getElementById('root')
 );
 
 ReactDOM.render(
-  <Carousel/>,
-  document.getElementById('root2')
+    <Carousel/>,
+    document.getElementById('root2')
+);
+
+ReactDOM.render(
+    <Email/>,
+    document.getElementById('root3')
 );
